639ae54866ae2fd7a1f55cec2a91783a89452d3c
changyue.li <changyue.li@tendcloud.com>
2020-02-24 12:56:34 +0800

    svm

---
+++ 5.SVM/4.Unbalance.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+import numpy as np
+from sklearn import svm
+import matplotlib.colors
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.exceptions import UndefinedMetricWarning
+import warnings
+
+#处理不平衡数据
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
